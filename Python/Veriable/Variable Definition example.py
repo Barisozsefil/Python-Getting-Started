b2e4fbafe9a8feda451653d8d44c01ff81c5788e
Barisozsefil <77811917+Barisozsefil@users.noreply.github.com>
2024-02-10 16:47:28 +0300

    .
    .

---
+++ Python/Veriable/Variable Definition example.py
@@ -0,0 +1,8 @@
+custumerName = "Baris"
+custumerSurname = "Öz"
+custumer = custumerName + custumerSurname
+custumerGender = True #Erkek
+custumerTCK = 123456789
+custumerBirthYear = "01 09 2023"
+custumerAddress = "Turkey"
+custumerAge = 20
---
+++ Python/Veriable/Variable Definition.py
@@ -0,0 +1,30 @@
+#Variables
+x = 1       #in 
+y = 2.1     #float  
+z = "Name"  #String
+t = True    #Bool
+
+
+#We created variables and used "int" and "float"
+
+maas1 = 1000
+maas2 = 2000
+vergi = 0.27
+
+print (maas1 - (maas1 * vergi))
+print (maas2 - (maas2 * vergi))
+
+
+#We created veriables and use string
+
+firstName = "Baris"
+lastName  = "Öz"
+
+print (firstName + lastName)
+
+#Assigning values in a single line
+
+x,y,z,t = (1,2,2.3,True)
+
+
+
