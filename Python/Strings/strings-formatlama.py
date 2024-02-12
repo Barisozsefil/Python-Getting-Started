d78bd9f10fb8f858f04d3489800c687eb467cbfd
Barisozsefil <77811917+Barisozsefil@users.noreply.github.com>
2024-02-12 14:33:26 +0300

    .
    .

---
+++ Python/Strings/strings-formatlama.py
@@ -0,0 +1,10 @@
+name = "Baris"
+surname = "Öz"
+age = 2
+# print ("My name is {} {}".format(name,surname)) 
+# print ("My name is {1} {0}".format(name,surname))
+# print ("My name is {s} {n}".format(n=name,s=surname))
+# print ("My name is {s} {n} and ı m{a}.".format(n=name,s=surname,a=age))
+print (f"My name is {name} {surname} and ı m{age}.")
+
+
