3fbb7b8cd04dfad32fd06538a1123200b555e209
Barisozsefil <77811917+Barisozsefil@users.noreply.github.com>
2024-02-12 18:20:50 +0300

    .

---
+++ Python/List/list-example.py
@@ -0,0 +1,79 @@
+"""
+    # Creating the "car" list
+    car = ["Bmw", "Mercedes", "Opel", "Mazda"]
+
+    # Calculating the length of the list and assigning it to the "result" variable
+    result = len(car)
+
+    # Assigning the first element of the list to the "result" variable
+    result = car[0]
+
+    # Replacing the last element of the list with "Toyota" and assigning it to the "result" variable
+    result = car[-1] = "Toyota"
+
+    # Checking if "Mercedes" is in the list and assigning the result to the "result" variable
+    result = "Mercedes" in car
+
+    # Assigning the second-to-last element of the list to the "result" variable
+    result = car[-2]
+
+    # Assigning the first three elements of the list to the "result" variable
+    result = car[:3]
+
+    # Replacing the last two elements of the list with "Toyota" and "Renoult"
+    car[-1] = "Toyota"
+    car[-2] = "Renoult"
+
+    # Adding "Audi" and "Nissan" to the list and assigning the result to the "result" variable
+    result = car + ["Audi", "Nisaan"]
+
+    # Deleting the last element of the list
+    del car[-1]
+
+    # Printing the final result
+    print(result)
+
+
+
+studentA =["Yiğit","Bilgi",2010,[70,60,70]]
+studentB =["Sena","Turan",1999,[80,80,70]]
+studentC =["Ahmet","Turan",1998,[80,70, 90]]
+
+schol=studentA[0]
+schol=studentB[2]
+schol=studentC[3][2]
+
+schol= f"{studentA[0]} budur {studentB[1]} budur {studentC[2]} budur ve büt notum{studentA[3][2]}"
+
+print (schol)
+
+
+
+numbers = [1,2,3,4,588,9,65,34]
+letters =["a", "sa","as","e","o"]
+
+val = min(numbers)
+val = max(numbers)
+val = max(letters)
+val = min(letters)
+
+numbers.append(49)
+numbers.insert(2,50)
+numbers.pop(0)
+numbers.remove(588)
+
+
+numbers.sort()
+
+print(numbers)
+
+
+names = ["Ali","Yağmur","Hakan","Deniz"]
+
+years = [1998,2000,1998,1987]
+
+names.append("Cenk")
+names.insert(0,"Sena")
+names.remove("Deniz")
+
+"""
---
+++ Python/List/list.py
@@ -0,0 +1,21 @@
+# message = "My name is Baris".split()
+# print (message)
+
+# my_list = [1, 2, 3]
+# my_list = [1, True , "baris", 0.1]
+# print (my_list)
+
+# list1= ["one" , "two", "three"]
+# list2= ["for","five", "six"]
+# numbers= list1 + list2
+
+
+# print(len(numbers))
+# print(numbers[2])
+
+
+# userA = ["Burak",37]
+# userb = ["Baris",56]
+# users=  [userA,userb,2]
+
+# print(users)
