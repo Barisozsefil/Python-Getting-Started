d3893dda0e2559ef7c97b6fafa132ac4cf47d19c
Barisozsefil <77811917+Barisozsefil@users.noreply.github.com>
2024-02-29 15:39:23 +0300

    .

---
+++ Python/Functions/function-demo.py
@@ -0,0 +1,68 @@
+# 1- Gönderilen bir kelimeyi belirtilen kez ekranda gösteren fonksiyonu yazın. 
+
+# def yazdir(kelime, adet):
+#     print(kelime * adet)
+
+# yazdir('Merhaba\n', 10)
+
+
+# def name (name1,adet):
+#     toplam = (f"Yazdığınız kelime {name1} ve adedi {adet} \n" )
+#     print (toplam*adet)
+
+
+# name("baris", 5 )
+
+
+# 2- Kendine gönderilen sınırsız sayıdaki parametreyi bir listeye çeviren fonksiyonu yazınız.
+
+# def listeyeCevir(*params):
+#     liste = []
+
+#     for param in params:
+#         liste.append(param)
+
+#     return liste
+
+# result = listeyeCevir(10,20,30,'Merhaba')
+
+# print(result)
+
+# def parametreler (*args):
+#     print (list(args))
+
+
+# parametreler("li" , 1, "ture")
+
+# 3- Gönderilen 2 sayı arasındaki tüm asal sayıları bulun.
+
+# def asalSayılariBul(sayi1, sayi2):
+#     for sayi in range(sayi1, sayi2+1):
+#         if sayi > 1:
+#             for i in range(2, sayi):
+#                 if (sayi % i == 0):
+#                     break
+#             else:
+#                 print(sayi)
+
+# sayi1 = int(input('sayı 1:'))
+# sayi2 = int(input('sayı 2:'))
+
+# asalSayılariBul(sayi1, sayi2)
+
+
+
+# 4- Kendisine gönderilen bir sayının tam bölenlerini bir liste şeklinde döndürünüz.
+
+
+def tamBolenleriBul(sayi):
+    tamBolenler = []
+
+    for i in range(2, sayi):
+        if (sayi % i == 0):
+            tamBolenler.append(i)
+    
+    return tamBolenler
+
+
+print(tamBolenleriBul(20))
