ed6d5464568818591f2f6d8b5b9daf150bee5cc4
Barisozsefil <77811917+Barisozsefil@users.noreply.github.com>
2024-03-10 14:30:13 +0300

    .

---
+++ Python/Modules/math.py
@@ -0,0 +1,42 @@
+################################################################
+# Yöntem 1
+
+
+# import math
+# import math as islem        => Takma ismi kullandık "as (gibi)" 
+
+# value = dir(math)
+# value = help(math)
+# value = help(math.factorial)
+# value = math.sqrt(49)
+# value = math.factorial(5)
+# value = math.floor(5.9)
+# value = math.ceil(5.9)
+################################################################
+
+
+
+
+################################################################
+# import math as islem  => Takma ismi kullandık "as (gibi)" 
+# value = islem.factorial(5)
+################################################################
+
+
+
+
+################################################################
+
+# Yöntem 2
+
+# from math import *
+
+from math import factorial,sqrt,ceil
+
+
+# value = factorial(5)
+# value = sqrt(9)
+# value = ceil(9.8)
+
+# print(value)
+################################################################
